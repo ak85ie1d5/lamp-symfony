c08585ed19ac084011d653bc72928b069ffd91bd
Jeremy SPAETH <ak85ie1d5@relay.firefox.com>
2025-03-27 10:27:11 +0100

    Adding script to deploy Symfony automaticaly on running

--- Docker/php/local-deploy.sh
+++ Docker/php/local-deploy.sh
@@ -8,12 +8,21 @@ if [ ! -f '.env.local' ]; then
   cat <<EOF > .env.local
 APP_ENV=dev
 APP_SECRET=$(openssl rand -base64 32)
+APP_DEBUG=1
+URL=${URL}
 DATABASE_URL="mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
-MAILER_DSN=
+MAILER_DSN=smtp://maildev:1025
 EOF
 fi
 
 # Install composer dependencies
 if [ ! -d 'vendor' ]; then composer install; fi
+
+# Install assets
+if [ -f 'importmap.php' ]; then
+  if [ ! -d 'public/bundles' ]; then php bin/console asset:install; fi
+
+  if [ ! -d 'public/assets' ]; then php bin/console asset-map:compile; fi
+fi
 
 exec "$@"
